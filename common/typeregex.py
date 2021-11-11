9434ad53a45228a1e9af6dc5c3a67b5e5b8001a4
Mizu <40831463+Rexxt@users.noreply.github.com>
2021-11-11 16:46:38 +0100

    Update booleans

--- common/typeregex.py
+++ common/typeregex.py
@@ -1,7 +1,11 @@
 # regular expressions for type checking
 string='".*"'
 number='\d+'
-boolean='(true|false)'
+bools={
+    True: 'true',
+    False: 'false'
+}
+boolean=f'({bools[True]}|{bools[False]})'
 name='[a-zA-Z_][a-zA-Z0-9_]*'
 nothing='nothing'
 expression='\(.*\)'
